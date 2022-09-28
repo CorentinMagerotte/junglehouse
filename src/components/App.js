edb0a141d4049bee20802142271afe7a487c6a33
CorentinMagerotte <magerotte.corentin@gmail.com>
2022-09-28 22:40:17 +0200

    Second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
+import Banner from "./Banner";
+import ShoppingList from "./ShoppingList";
+import logo from '../assets/logo.png'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+      <>
+          <Banner>
+              <img src={logo} alt='La maison jungle' className='lmj-logo' />
+              <h1 className='lmj-title'>La maison jungle</h1>
+          </Banner>
+          <ShoppingList/>
+      </>
   );
 }
 
